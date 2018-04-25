2a4b1fa0cc05780e5699b842aa2ccca6752931e3
root <root@daw2m08.fjeclot.net>
2018-04-25 19:04:18 +0200

    commit 2

--- peupagina.php
+++ peupagina.php
@@ -4,8 +4,16 @@ Plugin Name: Peu de pàgina per defecte per a noves entrades
 Plugin URI: Escriu l'adreça URL completa del teu plugin dins de Github
 Description: Crea peus de pàgina per defecte per a noves entrades
 Version: 0.1
-Author: El teu nom i cognom
+Author: Oriol Garcia Escudero
 Author URI: L'adreça URL del teu compte de Github
 License: GNU GPLv3
 */
+function admin_menu_peu_pagina(){
+	$titolPagina="Peu de pagina per defecte";
+	$titolEntrada="Peu de pagina per defecte";
+	$role="administrator";
+	$id="peu_pagina_per_defecte";
+	add_menu_page($titolPagina,$titolEntrada,$role,$id);
+}
+add_action('admin_menu','admin_menu_peu_pagina');
 ?>
